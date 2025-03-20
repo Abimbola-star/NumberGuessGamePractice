00436661916d866b2b1a7b163674f6158d8eb192
Abimbola-star <aroyewunbimbola@gmail.com>
2025-03-20 01:01:24 -0500

    Update NumberGuessServlet.java

--- src/main/java/com/studentapp/NumberGuessServlet.java
+++ src/main/java/com/studentapp/NumberGuessServlet.java
@@ -1,5 +1,4 @@
 package com.studentapp;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Random;
@@ -7,15 +6,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 public class NumberGuessServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private int targetNumber;
-    
     public void init() throws ServletException {
         targetNumber = new Random().nextInt(100) + 1;
     }
-    
+    public int getTargetNumber() {
+        return targetNumber;
+    }
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
@@ -26,7 +25,6 @@ public class NumberGuessServlet extends HttpServlet {
         out.println("<input type='submit' value='Submit' />");
         out.println("</form>");
     }
-    
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
